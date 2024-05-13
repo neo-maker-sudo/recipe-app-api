2c958d69eae87263ec24f1d89547b3fac8bbb425
Neo <eyywqkgb@gmail.com>
2024-05-13 15:42:56 +0800

    test(model): add create superuser test case

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,3 +34,12 @@ class ModelTests(TestCase):
             get_user_model().objects.create_user(
                 email="", password="Aa1234567"
             )
+
+    def test_create_superuser(self):
+
+        user = get_user_model().objects.create_superuser(
+            email="test1@example.com", password="Aa1234567"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
