c3a2dcb185dfad45e28c19ee033c6f58530e6726
Neo <eyywqkgb@gmail.com>
2024-05-17 22:31:05 +0800

    test(user): add user login test case

--- src/web/user/tests/test_user_api.py
+++ src/web/user/tests/test_user_api.py
@@ -9,6 +9,7 @@ from recipe.adapters import repository
 
 
 CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
 
 
 class PublicUserApiTests(TestCase):
@@ -20,6 +21,15 @@ class PublicUserApiTests(TestCase):
         self.password = "Aa1234567"
 
         self.repo = repository.UserRepository()
+
+    def create_user(self):
+        user = domain_model.User(
+            email=self.email, name=self.name, password=self.password
+        )
+
+        self.repo.add(user)
+
+        return user
 
     def test_create_user_success(self):
         user = domain_model.User(
@@ -38,11 +48,7 @@ class PublicUserApiTests(TestCase):
         self.assertTrue(user_dto.check_password(user.password))
 
     def test_create_user_exist_error(self):
-        user = domain_model.User(
-            email=self.email, name=self.name, password=self.password
-        )
-
-        self.repo.add(user)
+        user = self.create_user()
 
         res = self.client.post(
             CREATE_USER_URL,
@@ -65,3 +71,35 @@ class PublicUserApiTests(TestCase):
 
         with self.assertRaises(self.repo.model.DoesNotExist):
             self.repo.get(email=user.email)
+
+    def test_create_token_for_user(self):
+        user = self.create_user()
+
+        payload = dict(email=user.email, password=user.password)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("access_token", res.data)
+
+    def test_create_token_bad_credentials(self):
+        user = self.create_user()
+
+        payload = dict(email=user.email, password="pw")
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn("access_token", res.data)
+
+    def test_create_token_blank_password(self):
+        payload = dict(email=self.email, password="")
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn("access_token", res.data)
+
+    def test_create_token_user_not_exist(self):
+        payload = dict(email=self.email, password=self.password)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn("access_token", res.data)
