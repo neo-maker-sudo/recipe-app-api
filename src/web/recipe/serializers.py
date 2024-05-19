9316f508ec82a5599ec7c393cc384ce9438d3600
Neo <eyywqkgb@gmail.com>
2024-05-20 02:09:58 +0800

    test(recipe): add recipe list test case

---
+++ src/web/recipe/serializers.py
@@ -0,0 +1,40 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+from rest_framework_simplejwt.authentication import (
+    JWTStatelessUserAuthentication,
+)
+from rest_framework_simplejwt.exceptions import (
+    InvalidToken,
+)
+
+
+class JWTAuthSerializer(serializers.Serializer):
+    invalid_token_msg = "Token contained no recognizable user identification"
+
+    def validate(self, attrs):
+        request = self.context.get("request")
+
+        jwt_authenticator = JWTStatelessUserAuthentication()
+        result = jwt_authenticator.authenticate(request)
+
+        if result is None:
+            raise InvalidToken(_(self.invalid_token_msg))
+
+        token_user, payload = result
+        attrs["user_id"] = payload["user_id"]
+        return attrs
+
+
+class RecipeListSerializerIn(JWTAuthSerializer):
+
+    def validate(self, attrs):
+        return super().validate(attrs)
+
+
+class RecipeListSerializerOut(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    time_minutes = serializers.IntegerField()
+    price = serializers.DecimalField(max_digits=5, decimal_places=2)
+    link = serializers.CharField()
---
+++ src/web/recipe/tests/test_recipe_api.py
@@ -0,0 +1,105 @@
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+
+from recipe.serializers import RecipeListSerializerOut
+
+RECIPES_URL = reverse("recipe:recipe-list")
+TOKEN_URL = reverse("user:token")
+
+
+def create_recipe(user, **params):
+    default = {
+        "title": "recipe title",
+        "description": "recipe description",
+        "time_minutes": 25,
+        "price": Decimal("5.25"),
+        "link": "https://example.com/recipe.pdf",
+    }
+    default.update(params)
+
+    recipe = Recipe.objects.create(user=user, **default)
+    return recipe
+
+
+class PublicRecipeAPITests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        order_by = "id"
+        res = self.client.get(RECIPES_URL, **{"QUERY_STRING": f"o={order_by}"})
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateRecipeAPITests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.email = "user@example.com"
+        self.password = "Aa1234567"
+        self.user = get_user_model().objects.create_user(
+            self.email,
+            self.password,
+        )
+        self.other_user = get_user_model().objects.create_user(
+            "other@example.com",
+            "Aa1234567",
+        )
+
+        self.login()
+        self.headers = {
+            "HTTP_AUTHORIZATION": f"{self.token_type} {self.access_token}"
+        }
+
+    def login(self):
+        payload = dict(email=self.email, password=self.password)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.access_token = res.data["access_token"]
+        self.token_type = res.data["token_type"]
+
+    def test_retrieve_recipes(self):
+        order_by = "id"
+
+        create_recipe(self.user)
+        create_recipe(self.user)
+
+        res = self.client.get(
+            RECIPES_URL, **{"QUERY_STRING": f"o={order_by}"}, **self.headers
+        )
+
+        recipes = [
+            recipe.to_domain()
+            for recipe in Recipe.objects.all().order_by(order_by)
+        ]
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data, RecipeListSerializerOut(recipes, many=True).data
+        )
+
+    def test_recipe_list_limited_to_user(self):
+        order_by = "-id"
+
+        create_recipe(self.user)
+        create_recipe(self.other_user)
+
+        res = self.client.get(
+            RECIPES_URL, **{"QUERY_STRING": f"o={order_by}"}, **self.headers
+        )
+
+        recipes = Recipe.objects.filter(user=self.user).order_by(order_by)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data, RecipeListSerializerOut(recipes, many=True).data
+        )
