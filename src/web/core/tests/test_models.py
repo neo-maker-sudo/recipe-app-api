9392bcaffab0c9e08129d120bd8ea8927b2d3e9d
Neo <eyywqkgb@gmail.com>
2024-05-24 17:14:32 +0800

    test(image): add recipe filename test case

--- src/web/core/tests/test_models.py
+++ src/web/core/tests/test_models.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from unittest.mock import patch
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -82,3 +83,12 @@ class ModelTests(TestCase):
             user=user, name=ingredient_name
         )
         self.assertEqual(str(ingredient), ingredient_name)
+
+    @patch("core.models.uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "example.jpg")
+
+        self.assertEqual(file_path, f"uploads/recipe/{uuid}.jpg")
